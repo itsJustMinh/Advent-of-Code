1f972df0f65bc2d136246fff73413223870ade51
samminhch@stemtop <45446622+samminhch@users.noreply.github.com>
2023-12-11 19:33:55 -0600

    added day 1 input and ability to supply day & test state via command line arguments

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{env, fs::File, io::Write};
 
 mod day1;
 
@@ -6,54 +6,66 @@ const AOC_FUNCTIONS: [fn(File); 1] = [day1::solution];
 const INPUT_DIRECTORY: &str = "input/";
 
 fn main() {
-    let day: usize;
-    let test: bool;
+    let args = env::args().collect::<Vec<String>>();
 
-    loop {
-        let mut input = String::new();
-        print!("Which day would you like to do: ");
-        std::io::stdin().read_line(&mut input).unwrap();
-        input = input.trim().to_string();
-
-        match input.parse::<usize>() {
-            Ok(res) => {
-                if (1..=25).contains(&res) {
-                    day = res;
-                    break;
-                }
-            }
-            Err(_) => (),
-        }
-    }
-
-    loop {
-        let mut input = String::new();
-        print!("Are you using the test file [T] or final file [F]? ");
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        input = input.to_uppercase();
-
-        match input.as_str() {
-            "T" => {
-                test = true;
-                break;
-            }
-            "F" => {
-                test = false;
-                break;
-            }
-            _ => (),
-        }
-    }
+    let day = set_day(&args);
+    let test = set_test(&args);
 
     let file_path = format!(
         "{INPUT_DIRECTORY}day-{}{}.txt",
-        day - 1,
+        day,
         if test { "-test" } else { "" }
     );
 
     match File::open(&file_path) {
         Ok(aoc_file) => AOC_FUNCTIONS[day - 1](aoc_file),
         Err(_) => println!("Could not find {file_path}! Exiting..."),
+    }
+}
+
+fn set_day(args: &[String]) -> usize {
+    if let Some(day) = args
+        .iter()
+        .position(|arg| arg == "--day")
+        .and_then(|idx| args.get(idx + 1))
+        .and_then(|val| val.parse::<usize>().ok())
+        .filter(|&num| (1..=25).contains(&num))
+    {
+        return day;
+    }
+
+    loop {
+        let mut input = String::new();
+        print!("Which day would you like to do: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        input = input.trim().to_string();
+
+        if let Ok(res) = input.parse::<usize>() {
+            if (1..=25).contains(&res) {
+                return res;
+            }
+        }
+    }
+}
+
+fn set_test(args: &[String]) -> bool {
+    if let Some(test) = args.iter().position(|arg| arg == "--test").and_then(|idx| args.get(idx + 1)) {
+        return test == "true";
+    }
+
+    loop {
+        let mut input = String::new();
+        print!("Are you using the test file [T] or final file [F]? ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        input = input.trim().to_uppercase();
+
+        match input.as_str() {
+            "T" => return true,
+            "F" => return false,
+            _ => (),
+        }
     }
 }
